3cdcaa993f12afbaeef51af6a2e71922c62c5b1a
amomoh6392 <amomoh@byupathway.edu>
2026-09-13 20:12:22 +0100

    I corrected my mistake for the add to cart function in the utility.js

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -22,7 +22,7 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
-export function getParam(product) {
+export function getParam() {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const product = urlParams.get('product');
