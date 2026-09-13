6d161f6e7beea67daffb4e1c992a0132bfda8313
amomoh6392 <amomoh@byupathway.edu>
2026-09-13 21:22:57 +0100

    fixed the cart

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -30,8 +30,8 @@ function productDetailsTemplate(product) {
     productImage.src = product.Image;
     productImage.alt = product.NameWithoutBrand;
 
-    document.getElementById("productPrice").textContent = product.finalPrice;
-    document.getElementById("productColor").textContent = product.Color[0].ColorName;
+    document.getElementById("productPrice").textContent = product.FinalPrice;
+    document.getElementById("productColor").textContent = product.Colors[0].ColorName;
     document.getElementById("productDesc").innerHTML = product.DescriptionHtmlSimple;
 
     document.getElementById("addToCart").dataset.id = product.Id;
