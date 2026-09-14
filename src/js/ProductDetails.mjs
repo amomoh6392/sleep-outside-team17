518fb8211ae2603e881a5b9053567978fe90e6d9
amomoh6392 <amomoh@byupathway.edu>
2026-09-14 22:03:07 +0100

    trying to fix the page

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -24,7 +24,7 @@ export default class ProductDetails {
     }
 }
 function productDetailsTemplate(product) {
-    document.querySelector("h2").textContent = product.Brand.Name;
+    document.querySelector("h2").textContent = product.BrandName;
     document.querySelector("h3").textContent = product.NameWithoutBrand;
     const productImage = document.getElementById("productImage");
     productImage.src = product.Image;
