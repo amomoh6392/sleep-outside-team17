bc0ff0fac6348a708a928ef7629443573d0168b1
amomoh6392 <amomoh@byupathway.edu>
2026-09-17 16:34:46 +0100

    Dynamic Header and Footer Done

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,14 @@
 import ProductData from "./ProductData.mjs";
-import { getParam } from "./utils.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
 const product = new ProductDetails(productId, dataSource);
+
 product.init();
+
+loadHeaderFooter();
 
 //function addProductToCart(product) {
 // setLocalStorage("so-cart", product);
