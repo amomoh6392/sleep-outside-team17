2da3e3d1fd6ac5b876e69e494d3fa49559c51562
amomoh6392 <amomoh@byupathway.edu>
2026-09-21 12:07:58 +0100

    Individual Activity: Expand Inventory Done

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -22,11 +22,11 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
-export function getParam() {
+export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const product = urlParams.get("product");
-  return product;
+  
+  return urlParams.get(param);
 }
 
 export function renderListWithTemplate(
