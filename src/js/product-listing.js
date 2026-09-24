675e740d15e10ac8e9f0ff310dd9dacce3d63c6d
willims1992 <willsjuniorutopia@gmail.com>
2026-09-24 14:01:13 +0100

    change

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,5 +1,5 @@
 import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
+import ProductList from "./productlist.mjs";
 import { loadHeaderFooter, getParam } from "./utils.mjs";
 
 loadHeaderFooter();
