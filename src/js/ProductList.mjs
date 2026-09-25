21045130b442a0cc74011bfa73b9515242ccbc63
willims1992 <willsjuniorutopia@gmail.com>
2026-09-25 11:15:09 +0100

    wirking on some code

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -2,7 +2,7 @@ import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
     return`<li class="product-card">
-            <a href="/product_pages/index.html?product=${product.Id}">
+            <a href="../product_pages/index.html?product=${product.Id}">
                 <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.NameWithoutBrand}">
                 <h2 class="card__brand">${product.Brand.Name}</h2>
                 <h3 class="card__name">${product.NameWithoutBrand}</h3>
